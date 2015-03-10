a7b63628a5f0f5d31910da23f15c498de3fb5840
akeshwar <akeshwarj@gmail.com>
2015-03-10 16:54:41 +0530

    Create Linkedin.java

---
+++ Linkedin.java
@@ -0,0 +1,7 @@
+public class Linkedin implements LogIn{
+	public void login(){
+		System.out.println("opening the pop-up for login through LinkedIn");
+		
+		//further stuff can be done here.
+	}
+}
