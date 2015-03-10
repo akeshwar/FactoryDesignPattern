aafff9e4b51731898b3988165a77ba120a08ae93
akeshwar <akeshwarj@gmail.com>
2015-03-10 16:53:45 +0530

    Create Facebook.java

---
+++ Facebook.java
@@ -0,0 +1,7 @@
+public class Facebook implements LogIn{
+	public void login(){
+		System.out.println("opening the pop-up for login through Facebook");
+		
+		//further stuff can be done here.
+	}
+}
